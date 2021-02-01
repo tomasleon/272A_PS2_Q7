cefa4ddb196a052771ac425ae0b5d64888e254c9
dsconnell <32777425+dsconnell@users.noreply.github.com>
2021-02-01 10:00:23 -0800

    tweaked instructions

--- PS_2_Q7.R
+++ PS_2_Q7.R
@@ -6,15 +6,15 @@ wells <- read_csv("INSERT_PATH_HERE")
 #View your well file head
 head("FILL THIS IN")
 
-#Summarize number of wells located within 5km of other wells
+#Summarize number of wells located within 7 km of other wells
 summary("FILL THIS IN")
 
-#Box plot [note, you should consider changing y-axis scale and add labels]
+#Box plot [note, you should add labels and consider changing y-axis scale]
 ggplot("FILL THIS IN") + geom_boxplot("FILL THIS IN")
 
-#Histogram [note, you need to change axis labels and perhaps binwidth]
+#Histogram [note, you need to change axis labels and binwidth]
 ggplot("FILL THIS IN") + geom_histogram("FILL THIS IN") 
 
-#Filter
+#Filter wells that went into use after 2009
 wells_2009 <- filter("FILL THIS IN")
 "FILL THIS IN"(wells_2009)
